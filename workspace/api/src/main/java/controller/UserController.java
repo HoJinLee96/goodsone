3fc6caf04c2519daace1b66e1628322903c9ee8b
leehojin <tuy76534@gamil.com>
2024-06-23 22:13:46 +0900

    UserController 컨트롤러 작성 및 웹 흐름 다이어그램 작성

--- workspace/api/src/main/java/controller/UserController.java
+++ workspace/api/src/main/java/controller/UserController.java
@@ -4,10 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.goodsone1.core.dto.User;
-import core_service.UserService;
+import dto.User;
+import service.UserService;
 
 @RestController
 @RequestMapping("/users")
